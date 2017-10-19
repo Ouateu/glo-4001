c7cc10f8b472e2322fa8f38d5c75154b872f1100
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-10-19 19:02:08 -0400

    ajout code principal pour capteurs question 1

--- tp1/src/question1.py
+++ tp1/src/question1.py
@@ -1,3 +1,35 @@
-import numpy
-import scipy
+import numpy as np
+import pylab
+import scipy.io as scio
 
+
+GYRO_RATE = 25  # Hz
+
+
+def get_distance(measure):
+    return 1/measure
+
+
+def update_angular_position(actual_position, angular_speed):
+    return actual_position + angular_speed / GYRO_RATE
+
+
+def main():
+    data = scio.loadmat("Q1DonneesBon.mat")
+
+    distance_sensor_measures = np.array(data['z'][0])
+    gyro_measures = np.array(data['g'][0])
+
+    angular_position = 0
+
+    M = np.array([])
+    for angular_speed,distance_measure in (gyro_measures, distance_sensor_measures):
+        angular_position = update_angular_position(angular_position, angular_speed)
+        distance = get_distance(distance_measure)
+        M = np.vstack([M, [angular_position, distance]])
+
+    pylab.plot(M, 'ro')
+
+
+if __name__ == "__main__":
+    main()
