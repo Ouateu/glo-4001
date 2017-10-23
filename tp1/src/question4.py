d352ae30b66da1c22a7e2106a0b32a418ee3bb95
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-10-22 21:14:19 -0400

    resultat question 4

--- tp1/src/question4.py
+++ tp1/src/question4.py
@@ -4,25 +4,46 @@ import matplotlib.pyplot as plt
 import fast
 
 
-DEFAULT_THRESHOLD = 10
+class Corner:
+
+    def __init__(self, point, intensity):
+        self.point = point
+        self.intensity = intensity
+
+    def __eq__(self, other):
+        assert(isinstance(other, Corner))
+        return self.intensity == other.intensity
+
+    def __lt__(self, other):
+        assert(isinstance(other, Corner))
+        return self.intensity < other.intensity
+
+
+DEFAULT_THRESHOLD = 100
 
 
 def main():
     f = ndimage.imread("../res/bw-rectified-left-022146small.png")
 
-    corners = {}
-    max_v, max_u = f.shape
+    corners = []
+    max_u, max_v = f.shape
     print(f.shape)
-    plt.imshow(f, cmap='gray')
-    for v in range(max_v - 14):
-        for u in range(max_u - 14):
+    for u in range(max_u - 14):
+        print("Col: {}".format(u))
+        for v in range(max_v - 14):
             center = fast.Point(u, v)
             detector = fast.Fast(f, center, DEFAULT_THRESHOLD)
             is_corner, intensity = detector.detection_coin_fast()
             if is_corner:
-                corners[center] = intensity
-                plt.plot(v, u, 'ro')
+                corner = Corner(center, intensity)
+                corners.append(corner)
 
+    print("Nombre de coins: {}".format(len(corners)))
+    corners.sort()
+    xs = [corner.point.x for corner in corners]
+    ys = [corner.point.y for corner in corners]
+    plt.imshow(f, cmap='gray')
+    plt.plot(ys, xs, 'ro')
     plt.show()
 
 
