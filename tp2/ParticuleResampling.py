73abafe57291afa8c9d7c4489854ad96b6499298
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2017-12-02 16:40:29 -0500

    conversion de ParticuleResampling pour fonctionner avec python

--- tp2/ParticuleResampling.py
+++ tp2/ParticuleResampling.py
@@ -1,49 +1,65 @@
-function [X w] = ParticuleResampling(X,w,Ratio)
-% ParticuleResampling   Pour effectuer le reechantillonnage des particules,
-%                       si necessaire
-% [XResampled WResampled] = ParticuleResampling(X,w,Ratio)
-%   Entree
-%        X : matrice de DxN, ou N est le nombre de particules et D le
-%            nombre de variable d'etat
-%        w : matrice de 1xN, ou N est le nombre de particules
-%    Ratio : Ratio effectif en-deca duquel on reechantillonne. 
-%
-%  Sortie : X et w reechantillonnes
-%  Ver 1.1
+from collections import namedtuple
 
-    % Normaliser les poids w;
-    nParticules = size(w,2);
-    Wnorm = sum(w);
-    w = w./Wnorm;
-    Copy = zeros(1,nParticules);
+import numpy as np
+import math as m
+
+
+Resample = namedtuple('Resample', ['X', 'w'])
+
+def particule_resampling(X, w, ratio):
+    """
+        ParticuleResampling   Pour effectuer le reechantillonnage des particules,
+                              si necessaire
+        [XResampled WResampled] = ParticuleResampling(X,w,Ratio)
+          Entree
+               X : matrice de DxN, de type np.array, ou N est le nombre de particules et D le
+                   nombre de variable d'etat
+               w : matrice de 1xN, ou N est le nombre de particules
+           Ratio : Ratio effectif en-deca duquel on reechantillonne.
+
+         Sortie : X et w reechantillonnes
+         Ver 1.1
+    """
+
+    # Normaliser les poids w;
+    nParticules = len(w)
+    Wnorm = sum(w)
+    w = np.divide(w, Wnorm).tolist()
+    copy = [0] * nParticules
     
-    % Resampling, pour combattre l'appauvrissement
-    Neff = 1/sum(w.^2);
+    # Resampling, pour combattre l'appauvrissement
+    Neff = 1/sum([i**2 for i in w])
     
-    if (isnan(Neff))
-        Neff = 0;
-        w = ones(1,nParticules)./nParticules;
-    end
+    if m.isnan(Neff):
+        Neff = 0
+        w = np.divide(([1] * nParticules), nParticules).tolist()
+
 
    
-    % Verification si appauvrissement des particules
-    if (Neff < Ratio*nParticules)
-        % Effectuer un resampling.        
-        display('Resampling');
-        Q = cumsum(w);
-        T = sort(rand(1,nParticules+1));
-        T(nParticules+1) = 1;
-        index = 1; jindex = 1;
-        while (index <= nParticules)
-            if (T(index) < Q(jindex))
-                Copy(index) = jindex;
-                index = index + 1;
-            else
-                jindex = jindex + 1;
-            end
-        end
-        % Copie des particules, selon leur poids
-        w = ones(1,nParticules)./nParticules;
-        X = X(:,Copy);
-    end
-end
+    # Verification si appauvrissement des particules
+    if Neff < (ratio * nParticules):
+        # Effectuer un resampling.        
+        print('Resampling')
+
+        Q = np.cumsum(w).tolist()
+        sorted_array = np.sort(np.random.rand(nParticules+1), axis=None)
+        T = sorted_array.tolist()
+
+        T[nParticules] = 1
+
+        index = 0
+        jindex = 0
+
+        while index <= nParticules:
+            if T[index] < Q[jindex]:
+                copy[index] = jindex + 1
+                index = index + 1
+            else:
+                jindex = jindex + 1
+
+
+        # Copie des particules, selon leur poids
+        w = np.divide(([1] * nParticules), nParticules).tolist()
+        X = X[:, copy]
+
+        return Resample(X=X, w=w)
