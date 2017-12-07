017c70c41f773544ccf4b256fc7806b7eaf03f1f
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-12-07 12:53:00 -0500

    EKF semble mieux fonctionner

--- tp2/src/kalman.py
+++ tp2/src/kalman.py
@@ -11,10 +11,12 @@ myFontSize = 14  # Taille de la police de caractere pour les graphes
 d_init = 3  # Point de depart du robot
 S_angle = 0.003  # ecart-type du bruit sur la mesure d'angle.
 S_volt = 0.15  # ecart-type du bruit sur le voltage du moteur.
-Cv = np.matrix('0 {}'.format(2 * S_volt ** 2))  # bruit sur la propagation
-Cw = np.matrix('{} 0; 0 {}'.format(S_angle**2, S_angle**2))  # bruit sur la mesure
+S_speed = (2 * (1 / (1 + math.exp(-S_volt/2)) - 0.5))**2
+Cv = np.matrix('0; {}'.format(S_speed, S_speed))  # bruit sur la propagation
+Cw = np.matrix('{} 0; 0 0'.format(S_angle**2, S_angle**2))  # bruit sur la mesure
 
 number_steps = 400  # Nombres de mesures/pas
+# number_steps = 50  # Nombres de mesures/pas
 delta_t = 0.1  # Intervalle de temps entre les mesures/pas
 
 # Important! Il faut initialiser la matrice X de facon a avoir
@@ -23,8 +25,8 @@ X_vrai = [[d_init], [0.0]]  # Etat reel, inconnu du filtre et du robot.
 
 # Specifier les valeurs initiales des matrices.
 # Ne pas oublier qu'ici, ce sont des covariances, pas des ecarts-types.
-X = np.matrix('{}; 0'.format(d_init))  # un exemple d'initialisation.
-P = np.matrix('0 0; 0 0')
+X = np.matrix(np.array([[d_init+1], [0]]))
+P = np.matrix(np.array([[25, 0], [0, 1]]))
 
 Ax_vrai1 = []
 Ax_vrai2 = []
@@ -50,7 +52,7 @@ for step in range(1, number_steps):
 
     # Je simule pour vous la reponse de la camera.
     # Le max(0.001,...) est pour eviter les valeurs n�gatives.
-    z = max(0.001, 0.07 / X_vrai[1][0] + S_angle * np.random.normal())
+    z = max(0.001, 0.07 / X_vrai[0][0] + S_angle * np.random.normal())
     # =============== Fin de la simulation de deplacement reel ============   
 
     # ================ Debut de votre filtre E K F ou particule ==================
@@ -59,23 +61,22 @@ for step in range(1, number_steps):
     # elles vous sont inconnues.
 
     # Propagation
-    X[0] = X[0] + X[0] * delta_t
-    X[1] = 2 * (1 / (1 + math.exp(-U / 2)) - 0.5)
+    X[0, 0] = X[0] + X[1] * delta_t
+    X[1, 0] = 2 * (1 / (1 + math.exp(-U / 2)) - 0.5)
 
     # Jacobiennes
-    F = np.matrix('1 {}; 0, 0'.format(delta_t))  # Phi
-    G = np.matrix('0 {}'.format(math.exp(-U / 2) / (1 + math.exp(-U / 2)) ** 2))  # Gamma
-    H = np.matrix('{}; 0'.format(-0.07 / X[0]**2))  # Lambda
+    F = np.matrix(np.array([[1, delta_t], [0, 0]]))
+    G = np.matrix(np.array([[0], [math.exp(U / 2) / (1 + math.exp(U / 2)) ** 2]]))
+    H = np.matrix('{} 0'.format(-0.07 / X[0]**2))
 
-    P = np.multiply(np.multiply(F, P), F.transpose()) + np.multiply(np.multiply(G, Cv), G.transpose())  # covariance
+    P = F * P * F.T + G * G.T * S_volt**2
 
     # Mise a jour
-    denominator = np.multiply(np.multiply(H, P), H.transpose()) + Cw
-    K = np.multiply(np.multiply(P, H.transpose()), np.linalg.inv(denominator))  # Gain
-    z_predict = np.multiply(H, X)
+    K = (P * H.T) / (H * P * H.T + S_angle**2)
+    z_predict = 0.07/X[0, 0]
     r = z - z_predict
     X = X + K * r
-    P = np.multiply(np.eye(2) - np.multiply(K, H), P)
+    P = (np.eye(2) - K*H) * P
 
     # ========= Fin des equations du filtre EKF ou particule =============
 
@@ -92,9 +93,8 @@ for step in range(1, number_steps):
     plt.clf()
 
     plt.plot(ATime, AX1, 'go', label='EKF')
-    plt.plot(ATime, Ax_vrai1, 'k-', 'LineWidth', 2, label='Position Exacte')
-    plt.plot(ATime, np.divide(0.07, AZ).tolist(), 'r*',
-             label='Mesure h_z^{-1}')  # Ici on peut inverser le capteur, pour trouver la position correspondant a z.
+    plt.plot(ATime, Ax_vrai1, 'k-', label='Position Exacte')
+    plt.plot(ATime, np.divide(0.07, AZ).tolist(), 'r*', label='Mesure h_z^{-1}')
 
     plt.xlabel('Temps (s)')
     plt.ylabel('Estime de position (m)')
@@ -103,3 +103,4 @@ for step in range(1, number_steps):
     plt.ylim(0, 20)
 
     plt.draw()
+    plt.show()
